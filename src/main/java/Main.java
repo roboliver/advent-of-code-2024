a05e1e6dc2cbe198046745b499c7ad9f56e6dd54
Rob Oliver <oliver.rw93@gmail.com>
2024-12-04 16:31:05 +0000

    initial repo setup

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+
+    public static String hello() {
+        return "Hello world!";
+    }
+}
